6483899536d47a06acafbf8f962e3eb676131727
venkatadri489 <mallevenkatadri892@gmail.com>
2026-03-02 05:00:36 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./Rayalaseema/core/templates/**/*.html",
+    "./Rayalaseema/**/*.html",
+    "./Rayalaseema/core/static/**/*.js",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
